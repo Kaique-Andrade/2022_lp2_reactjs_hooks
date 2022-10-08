a0e124672d4c0704cb907a768bd87fa8b8a408c1
KAIQUE  DE ANDRADE ALMEIDA <17.01113-2@maua.br>
2022-10-08 12:56:03 -0300

    Apostila 8 - 2.8

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Accordion from './Accordion'
 
 const App = () => {
+  const itens = [
+    {
+      titulo: "Java",
+      conteudo: "Linguagem compilada e interpretada."
+    },
+    {
+      titulo: "Python",
+      conteudo: "Linguagem interpretada e dinamicamente tipada."
+    },
+    {
+      titulo: "Javascript",
+      conteudo: "Interpretada. Executa do lado do clinente e do lado do servidor também."
+    }
+  ]
   return (
     <div>
-      App
+      <Accordion itens={itens}/>
     </div>
   )
 }
