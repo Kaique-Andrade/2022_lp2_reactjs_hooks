1f752e729d41b502d11bda0bbc129ad2c4a91650
KAIQUE  DE ANDRADE ALMEIDA <17.01113-2@maua.br>
2022-10-15 12:40:05 -0300

    Finalizado a apostila 8 - React Hooks

--- src/components/Busca.js
+++ src/components/Busca.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { InputText } from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+import striptags from 'striptags'
 
 const Busca = () => {
   const [termoDeBusca, setTermoDeBusca] = useState('React')
@@ -25,7 +27,18 @@ const Busca = () => {
         )
         setResultados(data.query.search)
     }
-    fazBusca()
+    if (termoDeBusca && !resultados.length){
+        fazBusca()
+    }
+    else{
+        const timeoutID = setTimeout(() => {
+            if(termoDeBusca)
+                fazBusca()
+        }, 1000)
+        return () => {
+            clearTimeout(timeoutID)
+        }
+    }
   }, [termoDeBusca])
   return (
     <div>
@@ -35,6 +48,31 @@ const Busca = () => {
                 onChange={(e) => setTermoDeBusca(e.target.value)}
             />
         </span>
+        {
+            resultados.map((resultado) => (
+                // .my-2.border.border-1.border-400
+                <div
+                    key={resultado.pageid} 
+                    className="y-2 border border-1 border-400">
+                        {/* .border-bottom.border.border-1.border-400.p-2.text-center.font-bold*/}
+                        <div className="border-bottom border border-1 border-400 p-2 text-center font-bold">
+                            {resultado.title}
+                            <span>
+                                <Button
+                                    icon="pi pi-send"
+                                    className='ml-2 p-button-rounded p-button-secondary'
+                                    onClick={() => window.open(`
+                                    https://en.wikipedia.org?curid=${resultado.pageid}`)}
+                                    />
+                            </span>
+                        </div>
+                        {/* .p-2 */}
+                        <div className="p-2">
+                            <span dangerouslySetInnerHTML={{__html: resultado.snippet}}></span>
+                        </div>
+                </div>
+            ))
+        }
     </div>
   )
 }
